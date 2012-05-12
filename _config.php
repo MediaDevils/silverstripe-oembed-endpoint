1babe1bbb22af51a09cfbefae9b120eae7c7bd40
Justin Martin <justin@mediadevils.com>
2012-05-11 17:17:46 -0700

    URL parameter matching bugfix.

--- _config.php
+++ _config.php
@@ -3,9 +3,13 @@ if(isset($_GET['url']) && strpos($_SERVER['QUERY_STRING'], '&url=') !== false) {
 	$parts = explode('&', $_SERVER['QUERY_STRING']);
 	$url = array_shift($parts);
 	parse_str($url, $_GET);
-	$oembedurl = array_pop($parts);
-	$oembedurl = explode("=", $oembedurl);
-	$_GET['oembedurl'] = $oembedurl[1];
+	array_shift($parts);
+	
+	foreach($parts as $part) {
+		$part = explode("=", $part);
+		if($part[0] == "url") $_GET['oembedurl'] = $part[1];
+		else $_GET[$part[0]] = $part[1];
+	}
 }
 
 Director::addRules(10, array(
